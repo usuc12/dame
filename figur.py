8c850218cb4f01f96f895a460a25eba6a282af87
AlpacaTheUnicorn <75070153+AlpacaTheUnicorn@users.noreply.github.com>
2020-11-26 11:27:30 +0100

    Update figur.py

--- figur.py
+++ figur.py
@@ -1,1 +1,16 @@
+class Figur():
+    def __init__(self, dame, coords):
+        self.__dame = dame
+        self.__coords = coords
 
+    def get_d(self):
+        pass #in process
+    
+    def set_d(self):
+        pass #in process
+    
+    def get_coords(self):
+        pass #in process
+
+    def set_coords(self):
+        pass #in process
