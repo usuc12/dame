3008e21d183da44ae8c0438b26a7ee3f3cef88a0
Mahdi Ahmed <ahmedmahdi.5491@googlemail.com>
2020-12-01 21:48:59 +0100

    uml funktionen in GameHandler eingefügt

--- GameHandler.py
+++ GameHandler.py
@@ -1,1 +1,18 @@
 import board
+
+class GameHandler:
+    def __init__(self, current_player, selected, white_count, black_count):
+        self.current_player = current_player
+        self.selected = selected
+        self.white_count = white_count
+        self.black_count = black_count
+
+    def select(event): return
+
+    def controls(): return
+
+    def draw_diff(): return
+
+    def draw_selected(): return
+
+    def check_moves(): return
