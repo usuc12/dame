d7b3f4fee2e5592ebd632267640f93f0a8d935e9
notKatharsis <42143506+notKatharsis@users.noreply.github.com>
2020-12-07 14:57:21 +0100

    Update Board_bg.py
    delete figur

--- board1/Board_bg/Board_bg.py
+++ board1/Board_bg/Board_bg.py
@@ -1,15 +1,5 @@
 import tkinter as tk
-
-class Figur:
-    def __init__(self,player1,coords):
-        self.dame = False
-        self.coords = coords
-        self.player1 = player1
-    def get_d(self): return self.dame
-    def set_d(self,dame): self.dame = dame
-    def get_coords(self): return self.coords
-    def set_coords(self,coords): self.coords = coords
-    def get_player1(self): return self.player1
+from Figur import *
 
 class Board:
     def __init__(self,n,u):
