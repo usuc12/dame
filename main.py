e29dbdadb8479da1e29c1a2a38211a7e8eefd0b8
Mahdi Ahmed <ahmedmahdi.5491@googlemail.com>
2020-12-19 21:12:10 +0100

    add onClick function instead of normal lambda function

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from GameHandler import *
 board = board.Board(8, 80) 
 gameHandler = GameHandler()
 
-board.canvas.bind("<Button-1>", lambda event: gameHandler.select(event, board))
+def onClick(event):
+    gameHandler.select(event, board)
+
+board.canvas.bind("<Button-1>", lambda event: onClick(event))
 
 x = board.get_board()
 
